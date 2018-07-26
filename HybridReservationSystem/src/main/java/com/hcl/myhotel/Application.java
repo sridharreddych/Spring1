247582770b82d07fdcf8246611fc4eee16f0c83d
Pranavi Chapala.51472815 <pranavi.c@hcl.com>
2018-07-26 15:07:54 +0530

    committing h files

---
+++ HybridReservationSystem/src/main/java/com/hcl/myhotel/Application.java
@@ -0,0 +1,41 @@
+package com.hcl.myhotel;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author Sridhar reddy
+ *  This is developed in Spring Boot with backend being Mysql
+ *
+ */
+@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration  // Sprint Boot Auto Configuration
+@ComponentScan(basePackages = "com.hcl.myhotel")
+@EnableJpaRepositories("com.hcl.myhotel.dao.jpa") 
+public class Application extends SpringBootServletInitializer {
+
+    private static final Class<Application> applicationClass = Application.class;
+    private static final Logger log = LoggerFactory.getLogger(applicationClass);
+
+	public static void main(String[] args) {
+		SpringApplication.run(applicationClass, args);
+	}
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
+    
+
+}
